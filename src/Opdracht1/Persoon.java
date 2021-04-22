cf70c977c060fbd647ee2a57b058b7e32f6695b6
khaitam2001 <khaitam2001@gmail.com>
2021-04-22 14:10:35 +0200

    class Persoon toegevoegd

---
+++ src/Opdracht1/Persoon.java
@@ -0,0 +1,26 @@
+package Opdracht1;
+
+public class Persoon {
+    private String naam;
+    private double budget;
+
+    public Persoon(String Naam) {
+        naam = Naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double newBudget) {
+        budget = newBudget;
+    }
+
+    public String toString() {
+        return naam + " heeft een budget van " + budget;
+    }
+}
