2ca4d677b52c6030b0a9daaf69403b0ff9d8d25a
khaitam2001 <khaitam2001@gmail.com>
2021-04-24 13:32:10 +0200

    Game Class toegevoegd

---
+++ src/Opdracht1/Game.java
@@ -0,0 +1,26 @@
+package Opdracht1;
+
+public class Game {
+    private String naam;
+    private double Prijs;
+
+    public Game(String Naam, double prijs) {
+        this.naam = Naam;
+        this.Prijs = prijs;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getPrijs() {
+        return Prijs;
+    }
+
+    public boolean equals(Game newGame) {
+        if (this == newGame) return true;
+        if (newGame == null || getClass() != newGame.getClass()) return false;
+
+        return this.getNaam() == newGame.getNaam();
+    }
+}
