7d5d5e3f392a2b7e4d2cc65e00f8ff6521668769
khaitam2001 <khaitam2001@gmail.com>
2021-05-10 11:14:13 +0200

    Node class toevoegen

---
+++ src/Opdracht1/Node.java
@@ -0,0 +1,32 @@
+package Opdracht1;
+
+public class Node {
+    private String naam;
+    private Node NodeA;
+    private Node NodeB;
+
+    public Node(String naam) {
+        // Constructor waar naam wordt ingevoerd
+        this.naam = naam;
+    }
+
+    public Node getNodeA() {
+        // Getter methode voor node A
+        return NodeA;
+    }
+
+    public void setNodeA(Node nodeA) {
+        // Setter methode voor node A
+        NodeA = nodeA;
+    }
+
+    public Node getNodeB() {
+        // Getter methode voor node B
+        return NodeB;
+    }
+
+    public void setNodeB(Node nodeB) {
+        // Setter methode voor node B
+        NodeB = nodeB;
+    }
+}
